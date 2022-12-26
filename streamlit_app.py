9a2b0b7d075a144c0314ab7df3f2d8d12814c6cf
shekar Seetharaman <121071439+shekars2022@users.noreply.github.com>
2022-12-26 13:32:25 -0600

    Update streamlit_app.py
    Added the line to import snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import requests
+import snowflake.connector
 streamlit.title('My Parents new Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
