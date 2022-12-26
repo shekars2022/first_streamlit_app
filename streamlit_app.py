d36d99b87db7c3e23370f15d55ceb164a81aa839
shekar Seetharaman <121071439+shekars2022@users.noreply.github.com>
2022-12-26 14:54:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,13 @@ import streamlit
 import snowflake.connector
 from urllib.error import URLError
 
+streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Build your own Fruit smoothie')
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -24,12 +31,7 @@ streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-streamlit.title('My Parents new Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('Build your own Fruit smoothie')
+
 
 
 streamlit.dataframe(my_fruit_list)
